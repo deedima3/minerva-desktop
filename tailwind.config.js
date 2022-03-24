1836a519ef983e792d6208e0c975795edc8e3d9d
Gede Sudimahendra <54178249+deedima3@users.noreply.github.com>
2022-03-24 23:48:58 +0800

    [ enhance ] Adding favicon and fix on navbar
    Change size in navbar to better resemble the ui design

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
           'black-alt' : '#151821',
           'neutral' : '#FBFBFB',
         }
+      },
+      shadow : {
+        'sidebar' : '4px 0px 4px rgba(0, 0, 0, 0.25)'
       }
     },
   },
