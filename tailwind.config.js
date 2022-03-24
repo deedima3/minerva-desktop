1445cfb25bd853af2e19b16a6564d56b61489db9
sagungputri21 <sagungputri1530@gmail.com>
2022-03-24 18:17:25 +0800

    adding assets and brand color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      'brand' : {
+        'primary' : '#58F9C8',
+        'secondary' : '#5DFAFA',
+        'black-primary' : '#1A1D27',
+        'black-secondary' : '#111319',
+        'black-alt' : '#151821',
+        'neutral' : '#FBFBFB',
+      }
+    },
   },
   plugins: [],
 }
