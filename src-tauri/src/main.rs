1439c83cac6e6b90f440ce6e6eaa446fef3bade2
Gede Sudimahendra <54178249+deedima3@users.noreply.github.com>
2022-03-24 17:06:19 +0800

    [ enhance ] Add Splashscreen and tauri config

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,21 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+use tauri::Manager;
+// Create the command:
+// This command must be async so that it doesn't run on the main thread.
+#[tauri::command]
+async fn close_splashscreen(window: tauri::Window) {
+  // Close splashscreen
+  if let Some(splashscreen) = window.get_window("splashscreen") {
+    splashscreen.close().unwrap();
+  }
+  // Show main window
+  window.get_window("main").unwrap().show().unwrap();
+}
 
+// Register the command:
 fn main() {
   tauri::Builder::default()
+    // Add this line
+    .invoke_handler(tauri::generate_handler![close_splashscreen])
     .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    .expect("failed to run app");
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+// With the Tauri API npm package:
+import { invoke } from '@tauri-apps/api/tauri'
+// With the Tauri global script:
+document.addEventListener('DOMContentLoaded', () => {
+  // This will wait for the window to load, but you could
+  // run this function on whatever trigger you want
+  invoke('close_splashscreen')
+})
 
 ReactDOM.render(
   <React.StrictMode>
