5d4acc8e70dd097ee3df5284322f102dbbb49441
Gede Sudimahendra <54178249+deedima3@users.noreply.github.com>
2022-03-24 18:33:06 +0800

    [ fix ] Fix on last commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      'brand' : {
-        'primary' : '#58F9C8',
-        'secondary' : '#5DFAFA',
-        'black-primary' : '#1A1D27',
-        'black-secondary' : '#111319',
-        'black-alt' : '#151821',
-        'neutral' : '#FBFBFB',
+      colors : {
+        'brand' : {
+          'primary' : '#58F9C8',
+          'secondary' : '#5DFAFA',
+          'black-primary' : '#1A1D27',
+          'black-secondary' : '#111319',
+          'black-alt' : '#151821',
+          'neutral' : '#FBFBFB',
+        }
       }
     },
   },
