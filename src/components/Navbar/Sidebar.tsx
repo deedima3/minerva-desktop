7f0f9ca2e9aa9e597172325723a6b99648ce16bd
Gede Sudimahendra <54178249+deedima3@users.noreply.github.com>
2022-03-24 23:48:58 +0800

    [ enhance ] Adding favicon and fix on navbar
    Change size in navbar to better resemble the ui design

--- src/components/Navbar/Sidebar.tsx
+++ src/components/Navbar/Sidebar.tsx
@@ -8,18 +8,18 @@ import { Link } from "react-router-dom";
 const Sidebar = () => {
 
   return (
-    <div className="flex flex-col items-center bg-brand-black-primary min-h-screen w-24 py-5 shadow-lg justify-between">
+    <div className="flex flex-col items-center bg-brand-black-primary min-h-screen w-24 py-5 justify-between shadow-sidebar">
       <div className="flex flex-col items-center justify-center">
-        <img className="w-16" src={logo} alt="Minerva Logo" />
+        <img className="w-11" src={logo} alt="Minerva Logo" />
       </div>
       <div className="flex flex-col items-center justify-evenly h-72">
-        <SidebarLink routes="/" icon={<HomeOutlined style={{color : '#FBFBFB', fontSize: '32px'}} />} iconSelected={<HomeOutlined style={{color : '#58F9C8', fontSize: '32px'}}/>} /> 
-        <SidebarLink routes="/book" icon={<BookOutlined style={{color : '#FBFBFB', fontSize: '32px'}}/>} iconSelected={<BookOutlined style={{color : '#58F9C8', fontSize: '32px'}}/>} />
-        <SidebarLink routes="/book" icon={<UserOutlined style={{color : '#FBFBFB', fontSize: '32px'}}/>} iconSelected={<UserOutlined style={{color : '#58F9C8', fontSize: '32px'}}/>} /> 
+        <SidebarLink routes="/" icon={<HomeOutlined style={{color : '#FBFBFB', fontSize: '24px'}} />} iconSelected={<HomeOutlined style={{color : '#58F9C8', fontSize: '24px'}}/>} /> 
+        <SidebarLink routes="/book" icon={<BookOutlined style={{color : '#FBFBFB', fontSize: '24px'}}/>} iconSelected={<BookOutlined style={{color : '#58F9C8', fontSize: '24px'}}/>} />
+        <SidebarLink routes="/book" icon={<UserOutlined style={{color : '#FBFBFB', fontSize: '24px'}}/>} iconSelected={<UserOutlined style={{color : '#58F9C8', fontSize: '24px'}}/>} /> 
       </div>
       <div className="flex flex-col items-center justify-center">
         <Link to={"/admin"}>
-          <img src={user} alt="" className="rounded-full w-16"/>
+          <img src={user} alt="" className="rounded-full w-10"/>
         </Link>
       </div>
     </div>
